86e0eabad53d07a79351757f1d5dd1a9da836908
eroeum <eroeum@gmail.com>
2018-02-26 08:13:08 -0500

    Adds initial comments to core.py
    The core.py comments are intended to initialize what is expected of the
    bank program.
    This commit is mainly to add the working branch- next.

---
+++ sample/core.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    Runs the core of the program
+    Integrates all other python scripts/modules
+    Use the core.py to run the Bank python script
+    """
+    return 1
+
+if __name__ == '__main__':
+    main()
