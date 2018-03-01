275b6427730892da6a0b90057350f45d8290a0d4
eroeum <eroeum@gmail.com>
2018-03-01 15:14:28 -0500

    Adds ability to authenticate a password combo.
    Authenticates passwords using UUID and SHA512 password hashing.
    The class will store the salt, created by the UUID.
    The username and password will be hashed that cannot be reversed so that
    the username must be hashed each time it is authenticated.
    The password and username cannot be unhashed.
    
    Returns 1 if successful
    Returns 0 if the name was already taken.

--- sample/passwordAuthentication.py
+++ sample/passwordAuthentication.py
@@ -10,3 +10,36 @@ class Password:
 
         self.__salt = uuid.uuid4().hex
         self.__approvedUsers = approvedUsers
+
+    def authenticate_password(self,userid,password):
+        """
+        Autheticates Passwords using UUID and SHA512 password hashing
+
+        :type userid: string
+        :param userid: User ID that user has chosen
+
+        :type password: string
+        :param password: Password to attempt to authenticate userid
+        """
+
+        # Encodes userid and password into utf-8 format to be hashed
+        encodedUserID = (userid + self.__salt).encode('utf-8')
+        encodedPassword = (password + self.__salt).encode('utf-8')
+
+        # Hash (1-way) userid using SHA512
+        hashed_userid = hasher.sha512(encodedUserID).hexdigest()
+
+        # Detects if userid is regestered
+        if(hashed_userid not in self.__approvedUsers):
+            return 0
+
+        # Hash (1-way) password using SHA512
+        hashed_password = hasher.sha512(encodedPassword).hexdigest()
+
+        # Determines if the password matches the userid
+        if(self.__approvedUsers[hashed_userid] == hashed_password):
+            # Returns 1 if the password has been authenticated
+            return 1
+        else:
+            # Returns 0 if the password has not been authenticated
+            return 0
