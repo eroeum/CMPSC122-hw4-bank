48764720e4b1fc0b76484f68872fc88a68e57c90
eroeum <eroeum@gmail.com>
2018-03-01 18:09:04 -0500

    Adds ability to choose interface
    Use '-t' or '-no-gui' to run terminal interface

--- sample/core.py
+++ sample/core.py
@@ -1,10 +1,21 @@
-def main():
+import sys
+import terminalInterface as ti
+
+version = '0.1'
+
+def main(arg):
     """
     Runs the core of the program
     Integrates all other python scripts/modules
     Use the core.py to run the Bank python script
+
+    :type arg: list
+    :param arg: list of arguments in terminal
     """
-    return 1
+
+    # Displays terminal based application
+    if('--no-gui' in arg or '-t' in arg):
+        ti.displayInterface()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
