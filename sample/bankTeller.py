e2771eb7cfbb948b8e51a09c13297f1ad768a679
eroeum <eroeum@gmail.com>
2018-03-07 17:08:17 -0500

    Initializes variables associated with a bank teller
    Creates the varaibles associated with this class.
    The bank teller is a child of the customer class.
    Other params include:
    * customers: list of customers that can be accessed

---
+++ sample/bankTeller.py
@@ -0,0 +1,18 @@
+class BankTeller(Customer):
+    """
+    Represents a user with mid-level priveledges
+    Users having these priveledges are usually bank tellers
+    """
+    def __init__(self, userID, customers):
+        """
+        Creates the variables associated with this class
+
+        :type userID: string
+        :param userID: the owner of the account
+
+        :type customers: list
+        :param customers: all customers that can be accessed
+        """
+
+        Customer.__init__(self, 0 , userID)
+        self.__customers = customers
