72117435c5cf012932c47772f93dbb9a1e12cda4
eroeum <eroeum@gmail.com>
2018-03-07 21:41:37 -0500

    Creates the variables associated with assistants
    The assistant class is a child of the bankteller, a child of customer
    Other associative variables include bankTeller: a list of all
    bankTellers

---
+++ sample/assistant.py
@@ -0,0 +1,21 @@
+class Assistant(BankTeller):
+    """
+    Represents a user with mid-level priveledges
+    Users having these priveledges are usually bank tellers
+    """
+    def __init__(self, userID, customers, bankTellers):
+        """
+        Creates the variables associated with this class
+
+        :type userID: string
+        :param userID: the owner of the account
+
+        :type customers: list
+        :param customers: all customers that can be accessed
+
+        :type bankTellers: list
+        :param bankTellers: all bank tellers that can be accessed
+        """
+
+        BankTeller.__init__(self, userID, customers)
+        self.__bankTellers = bankTellers
