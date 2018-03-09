bed3718561aeb03edd16b5ae02b4a5daf76f77f3
eroeum <eroeum@gmail.com>
2018-03-08 21:00:51 -0500

    Initializes the variables associated with manager
    Createss the variables associated with the manager class.
    Manager is a child class of the assistant class.
    Parmaters are:
    * userID: Owner of the account
    * customers: all customers that can be accessed
    * bankTellers: all banktellers that can be accessed
    * assistnats: all assistants that can be accessed

---
+++ sample/manager.py
@@ -0,0 +1,24 @@
+class Manager(Assistant):
+    """
+    Represents a user with high-level priveledges
+    Users having these priveledges are usually managers
+    """
+    def __init__(self, userID, customers, bankTellers, assistants):
+        """
+        Creates the variables associated with this class
+
+        :type userID: string
+        :param userID: the owner of the account
+
+        :type customers: list
+        :param customers: all customers that can be accessed
+
+        :type bankTellers: list
+        :param bankTellers: all bank tellers that can be accessed
+
+        :type assistants: list
+        :param assistants: all assistants that can be accessed
+        """
+
+        Assistant.__init__(self, userID, customers, bankTellers)
+        self.__assistants = assistants
